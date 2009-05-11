ccd39004abc4c5ade0103a6cf17144443745f2cf
Ted Gould <ted@canonical.com>
2009-05-11 13:29:10 -0500

    Moving the glib layout test into the make file

--- tests/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo dbus-test-runner -d /usr/share/dbus-test-runner/session.conf --task ./test-glib-layout-client --task ./test-glib-layout-server
-dbus-test-runner -d /usr/share/dbus-test-runner/session.conf --task ./test-glib-layout-client --task ./test-glib-layout-server
