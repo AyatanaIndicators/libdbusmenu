887632f6fa0a1f0b5050ec79fd3be441b6e9f515
Ted Gould <ted@gould.cx>
2009-12-09 14:51:58 -0600

    Removing debug message

--- libdbusmenu-glib/client.c
+++ libdbusmenu-glib/client.c
@@ -790,8 +790,6 @@ update_layout_cb (DBusGProxy * proxy, guint rev, gchar * xml, GError * error, vo
 		return;
 	}
 
-	g_debug("Got layout: %s", xml);
-
 	priv->my_revision = rev;
 	/* g_debug("Root is now: 0x%X", (unsigned int)priv->root); */
 	priv->layoutcall = NULL;
