339b68e8b1a816a9ee5d44fea3a98e0492c65885
Ted Gould <ted@gould.cx>
2009-12-17 14:25:32 -0600

    License headers.  I hate these.

--- libdbusmenu-gtk/genericmenuitem.h
+++ libdbusmenu-gtk/genericmenuitem.h
@@ -1,3 +1,31 @@
+/*
+A menuitem subclass that has the ability to do lots of different
+things depending on it's settings.
+
+Copyright 2009 Canonical Ltd.
+
+Authors:
+    Ted Gould <ted@canonical.com>
+
+This program is free software: you can redistribute it and/or modify it 
+under the terms of either or both of the following licenses:
+
+1) the GNU Lesser General Public License version 3, as published by the 
+Free Software Foundation; and/or
+2) the GNU Lesser General Public License version 2.1, as published by 
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranties of 
+MERCHANTABILITY, SATISFACTORY QUALITY or FITNESS FOR A PARTICULAR 
+PURPOSE.  See the applicable version of the GNU Lesser General Public 
+License for more details.
+
+You should have received a copy of both the GNU Lesser General Public 
+License version 3 and version 2.1 along with this program.  If not, see 
+<http://www.gnu.org/licenses/>
+*/
+
 #ifndef __GENERICMENUITEM_H__
 #define __GENERICMENUITEM_H__
 
