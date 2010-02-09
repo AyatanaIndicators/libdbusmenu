db0e5c1bd674d437325638181ef0467855b19878
Ted Gould <ted@gould.cx>
2010-02-09 11:42:16 -0600

    License headers

--- libdbusmenu-glib/menuitem-proxy.h
+++ libdbusmenu-glib/menuitem-proxy.h
@@ -1,3 +1,31 @@
+/*
+An object to ferry over properties and signals between two different
+dbusmenu instances.  Useful for services.
+
+Copyright 2010 Canonical Ltd.
+
+Authors:
+    Ted Gould <ted@canonical.com>
+
+This program is free software: you can redistribute it and/or modify it 
+under the terms of either or both of the following licenses:
+
+1) the GNU Lesser General Public License version 3, as published by the 
+Free Software Foundation; and/or
+2) the GNU Lesser General Public License version 2.1, as published by 
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranties of 
+MERCHANTABILITY, SATISFACTORY QUALITY or FITNESS FOR A PARTICULAR 
+PURPOSE.  See the applicable version of the GNU Lesser General Public 
+License for more details.
+
+You should have received a copy of both the GNU Lesser General Public 
+License version 3 and version 2.1 along with this program.  If not, see 
+<http://www.gnu.org/licenses/>
+*/
+
 #ifndef __DBUSMENU_MENUITEM_PROXY_H__
 #define __DBUSMENU_MENUITEM_PROXY_H__
 
