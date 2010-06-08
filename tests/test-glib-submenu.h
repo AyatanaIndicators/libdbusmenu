915d4d952f7c2deb8e3f851f4144e203ec686333
Ted Gould <ted@gould.cx>
2010-06-08 14:18:36 -0500

    Making the layouts useful for us.

--- tests/test-glib-submenu.h
+++ tests/test-glib-submenu.h
@@ -25,54 +25,30 @@ struct _layout_t {
 	layout_t * submenu;
 };
 
-layout_t submenu_2[] = {
-	{id: 2, submenu: NULL},
-	{id: 3, submenu: NULL},
+layout_t submenu_l2[] = {
+	{id: 6, submenu: NULL},
+	{id: 7, submenu: NULL},
+	{id: 8, submenu: NULL},
 	{id: -1, submenu: NULL}
 };
-layout_t submenu_3_1[] = {
-	{id: 3, submenu: NULL},
+
+layout_t submenu[] = {
+	{id: 2, submenu: submenu_l2},
+	{id: 3, submenu: submenu_l2},
+	{id: -1, submenu: NULL}
+};
+
+layout_t no_submenu[] = {
 	{id: 4, submenu: NULL},
 	{id: 5, submenu: NULL},
 	{id: -1, submenu: NULL}
 };
-layout_t submenu_3_2[] = {
-	{id: 7, submenu: NULL},
-	{id: 8, submenu: NULL},
-	{id: 9, submenu: NULL},
-	{id: -1, submenu: NULL}
-};
-layout_t submenu_3[] = {
-	{id: 2, submenu: submenu_3_1},
-	{id: 6, submenu: submenu_3_2},
-	{id: -1, submenu: NULL}
-};
-layout_t submenu_4_1[] = {
-	{id: 6, submenu: NULL},
-	{id: -1, submenu: NULL}
-};
-layout_t submenu_4_2[] = {
-	{id: 5, submenu: submenu_4_1},
-	{id: -1, submenu: NULL}
-};
-layout_t submenu_4_3[] = {
-	{id: 4, submenu: submenu_4_2},
-	{id: -1, submenu: NULL}
-};
-layout_t submenu_4_4[] = {
-	{id: 3, submenu: submenu_4_3},
-	{id: -1, submenu: NULL}
-};
-layout_t submenu_4_5[] = {
-	{id: 2, submenu: submenu_4_4},
-	{id: -1, submenu: NULL}
-};
 
 layout_t layouts[] = {
-	{id: 5, submenu: NULL},
-	{id: 1, submenu: submenu_2},
-	{id: 1, submenu: submenu_3},
-	{id: 1, submenu: submenu_4_5},
+	{id: 1, submenu: no_submenu},
+	{id: 1, submenu: submenu},
+	{id: 1, submenu: no_submenu},
+	{id: 1, submenu: submenu},
 	{id: -1, submenu: NULL}
 };
 
