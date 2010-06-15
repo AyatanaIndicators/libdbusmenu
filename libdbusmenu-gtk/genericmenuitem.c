d0bbb5a0985adccf54cf857d4b0f17a8ba803cd2
Ted Gould <ted@gould.cx>
2010-06-15 13:50:52 -0500

    Setting the accel widget for the accel label.

--- libdbusmenu-gtk/genericmenuitem.c
+++ libdbusmenu-gtk/genericmenuitem.c
@@ -196,6 +196,7 @@ set_label (GtkMenuItem * menu_item, const gchar * label)
 		labelw = GTK_LABEL(gtk_accel_label_new(label));
 		gtk_label_set_use_underline(GTK_LABEL(labelw), TRUE);
 		gtk_misc_set_alignment(GTK_MISC(labelw), 0.0, 0.5);
+		gtk_accel_label_set_accel_widget(GTK_ACCEL_LABEL(labelw), GTK_WIDGET(menu_item));
 		gtk_widget_show(GTK_WIDGET(labelw));
 
 		/* Check to see if it needs to be in the bin for this
