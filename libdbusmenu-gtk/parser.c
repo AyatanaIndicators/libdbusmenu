c644e10c9da09e443a00422df00a6ba9df7a7e83
Ted Gould <ted@gould.cx>
2011-01-23 12:45:05 -0600

    Adding the base files for the parser

---
+++ libdbusmenu-gtk/parser.c
@@ -0,0 +1,9 @@
+
+#include "parser.h"
+
+DbusmenuMenuitem *
+dbusmenu_gtk_parse_menu_structure (GtkMenuItem * mi)
+{
+
+	return NULL;
+}
---
+++ libdbusmenu-gtk/parser.h
@@ -0,0 +1,6 @@
+
+#include <libdbusmenu-glib/menuitem.h>
+#include <gtk/gtk.h>
+
+DbusmenuMenuitem * dbusmenu_gtk_parse_menu_structure (GtkMenuItem * mi);
+
