02d6bd3f749237e4361845368eb96a0250212a93
Ted Gould <ted@gould.cx>
2011-02-17 20:48:38 -0600

    Whitespace

--- libdbusmenu-glib/server.h
+++ libdbusmenu-glib/server.h
@@ -108,11 +108,13 @@ struct _DbusmenuServer {
 	DbusmenuServerPrivate * priv;
 };
 
-GType               dbusmenu_server_get_type   (void);
-DbusmenuServer *    dbusmenu_server_new        (const gchar * object);
-void                dbusmenu_server_set_root   (DbusmenuServer * server, DbusmenuMenuitem * root);
-DbusmenuTextDirection dbusmenu_server_get_text_direction  (DbusmenuServer * server);
-void  dbusmenu_server_set_text_direction  (DbusmenuServer * server, DbusmenuTextDirection dir);
+GType                   dbusmenu_server_get_type            (void);
+DbusmenuServer *        dbusmenu_server_new                 (const gchar *          object);
+void                    dbusmenu_server_set_root            (DbusmenuServer *       server,
+                                                             DbusmenuMenuitem *     root);
+DbusmenuTextDirection   dbusmenu_server_get_text_direction  (DbusmenuServer *       server);
+void                    dbusmenu_server_set_text_direction  (DbusmenuServer *       server,
+                                                             DbusmenuTextDirection  dir);
 
 /**
 	SECIONT:server
