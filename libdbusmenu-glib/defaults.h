cb9011e136912751fb01d2107435e3422863cbdc
Ted Gould <ted@gould.cx>
2011-02-21 15:42:08 -0600

    Setting up the private and some base documentation

--- libdbusmenu-glib/defaults.h
+++ libdbusmenu-glib/defaults.h
@@ -13,18 +13,34 @@ G_BEGIN_DECLS
 #define IS_DBUSMENU_DEFAULTS_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), DBUSMENU_TYPE_DEFAULTS))
 #define DBUSMENU_DEFAULTS_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), DBUSMENU_TYPE_DEFAULTS, DbusmenuDefaultsClass))
 
-typedef struct _DbusmenuDefaults      DbusmenuDefaults;
-typedef struct _DbusmenuDefaultsClass DbusmenuDefaultsClass;
+typedef struct _DbusmenuDefaults        DbusmenuDefaults;
+typedef struct _DbusmenuDefaultsClass   DbusmenuDefaultsClass;
+typedef struct _DbusmenuDefaultsPrivate DbusmenuDefaultsPrivate;
 
+/**
+ * DbusmenuDefaultsClass:
+ *
+ * All of the signals and functions for #DbusmenuDefaults
+ */
 struct _DbusmenuDefaultsClass {
 	GObjectClass parent_class;
 };
 
+/**
+ * DbusmenuDefaults:
+ *
+ * A singleton to hold all of the defaults for the menuitems
+ * so they can use those easily.
+ */
 struct _DbusmenuDefaults {
 	GObject parent;
+
+	/*< Private >*/
+	DbusmenuDefaultsPrivate * priv;
 };
 
-GType dbusmenu_defaults_get_type (void);
+GType                 dbusmenu_defaults_get_type             (void);
+DbusmenuDefaults *    dbusmenu_defaults_ref_default          (void);
 
 G_END_DECLS
 
